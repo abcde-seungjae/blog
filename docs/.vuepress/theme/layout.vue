4aa47b95bfd6bd60fb995a0ce1506aab4a97f565
이승재 <sjlee@digitalship.co.kr>
2021-10-26 17:58:34 +0900

    커스텀 디자인 추가

---
+++ docs/.vuepress/theme/layout.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="theme-container">
+    <Content class="theme-default-content" />
+  </div>
+</template>
+<style lang="stylus" src="@vuepress/theme-default/styles/index.styl"></style>
---
+++ docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="global-layout">
+    <component :is="$page.path ? 'Layout' : 'NotFound'" />
+    <footer class="siteFooter">
+      &copy; ㅎㅇㅎㅇ
+    </footer>
+  </div>
+</template>
